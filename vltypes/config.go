aaa06fe1b07fc0fe3e70f9b7d8531da4a3b523f2
Artur Troian <troian.ap@gmail.com>
2019-10-10 21:53:18 +0300

    feat: vltypes package
    Signed-off-by: Artur Troian <troian.ap@gmail.com>

---
+++ vltypes/config.go
@@ -0,0 +1,32 @@
+package vltypes
+
+import (
+	"errors"
+)
+
+var (
+	ErrInvalidConfigType = errors.New("vltypes: invalid type of config object")
+)
+
+// NormalizeConfig make sure config object meets basic requirement to be as map[string]interface{}
+
+func NormalizeConfig(cfg interface{}) (map[string]interface{}, error) {
+	switch r := cfg.(type) {
+	case map[string]interface{}:
+		return r, nil
+	case map[interface{}]interface{}:
+		res := make(map[string]interface{})
+		for k, v := range r {
+			key, ok := k.(string)
+			if !ok {
+				return nil, ErrInvalidConfigType
+			}
+
+			res[key] = v
+		}
+
+		return res, nil
+	}
+
+	return nil, ErrInvalidConfigType
+}
